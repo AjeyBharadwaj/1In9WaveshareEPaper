2679ab676ceafb8f3fe7242a9bad2ec5850fd9c7
Ajey Bharadwaj A <chi.ajeyanantharamaiah@gmail.com>
2023-05-25 23:26:50 +0530

    With necessary files

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo apt-get install python3-pil
+sudo apt-get install python3-numpy
+sudo pip3 install RPi.GPIO
+sudo apt-get install python3-smbus
+sudo apt-get update
+sudo apt-get install python-pip
+sudo apt-get install python-pil
+sudo apt-get install python-numpy
+sudo pip install RPi.GPIO
+sudo apt-get install python-smbus
